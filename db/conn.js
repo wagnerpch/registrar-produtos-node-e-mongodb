1c1033ab5c547c4a1cca9c2c8d1f1bf9474182d3
t-wagner.chequeleiro <t-wagner.chequeleiro@cni.com.br>
2024-12-03 15:52:22 -0300

    feat: readme and connection setup for mongodb updated

--- db/conn.js
+++ db/conn.js
@@ -1,18 +1,19 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://127.0.0.1:27017/testemongodb"
+const uri =
+  "mongodb://root:example@127.0.0.1:27017/testemongodb?authSource=admin";
 
-const client = new MongoClient(uri)
+const client = new MongoClient(uri);
 
 async function run() {
-    try{
-        await client.connect()
-        console.log('Conectado')
-    }catch(error) {
-        console.log(error)
-    }
+  try {
+    await client.connect();
+    console.log("Conectado");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-run()
+run();
 
-module.exports = client
+module.exports = client;
